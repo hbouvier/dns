f20933ab87db2c2e7ad637b6ac82b98a6b9d73ed
Henri Bouvier <henri@omsignal.com>
2014-06-29 10:58:50 -0400

    Added REST DELELE for all entries when using the "force=true" parameter

--- public/js/controllers/dns-controller.js
+++ public/js/controllers/dns-controller.js
@@ -6,24 +6,30 @@ angular.module('dnsControllers', ['dnsServices', 'dnsModels'])
         };
         socket.on('new:host', function (host) {
         	var found = false;
-        	for (var i = 0 ; i < $scope.dns.hosts.addresses.length ; ++i) {
-        		if ($scope.dns.hosts.addresses[i].name === host.name) {
-        			found = true;
-        			$scope.dns.hosts.addresses[i].address = host.address;
-        			break;
-        		}
-        	}
+        	if ($scope.dns.hosts.addresses) {
+	        	for (var i = 0 ; i < $scope.dns.hosts.addresses.length ; ++i) {
+	        		if ($scope.dns.hosts.addresses[i].name === host.name) {
+	        			found = true;
+	        			$scope.dns.hosts.addresses[i].address = host.address;
+	        			break;
+	        		}
+	        	}
+	        }
         	if (!found) {
+        		if (!$scope.dns.hosts.addresses)
+        			$scope.dns.hosts.addresses = [];
         		$scope.dns.hosts.addresses.push(host);
         	}
         });
         socket.on('del:host', function (host) {
-        	for (var i = 0 ; i < $scope.dns.hosts.addresses.length ; ++i) {
-        		if ($scope.dns.hosts.addresses[i].name === host.name) {
-        			$scope.dns.hosts.addresses.splice(i, 1);
-        			break;
-        		}
-        	}
+        	if ($scope.dns.hosts.addresses) {
+	        	for (var i = 0 ; i < $scope.dns.hosts.addresses.length ; ++i) {
+	        		if ($scope.dns.hosts.addresses[i].name === host.name) {
+	        			$scope.dns.hosts.addresses.splice(i, 1);
+	        			break;
+	        		}
+	        	}
+	        }
         });
 
     })
